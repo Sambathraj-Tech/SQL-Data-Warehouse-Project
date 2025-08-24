0f27405621e136dbbf509815d6c6260ea71fe41c
Sambath Raj <137758810+Sambathraj-Tech@users.noreply.github.com>
2025-08-24 16:26:04 +0530

    Update DDL_Silver.sql

--- Scripts/Silver/DDL_Silver.sql
+++ Scripts/Silver/DDL_Silver.sql
@@ -1,7 +1,17 @@
 =========================================================================================================
   ADD DWH_CREATION_DATE: (To All the tables)
 =========================================================================================================
-  
+  /*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info
 CREATE TABLE silver.crm_cust_info(
